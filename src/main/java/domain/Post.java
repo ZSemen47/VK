2bf9a59a6e8b4faf8c0282bc04fe72bc4643fd1c
Semen Zazulin <zazulinsemen@gmail.com>
2020-11-01 13:17:20 +0300

    Initial commit

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Post {
+    private String id;
+    private String imgUrl;
+    private String hyperLink;
+    private String dateAndTime;
+    private String arrow;
+    private String postInfo;
+    private String like;
+    private String repost;
+}
