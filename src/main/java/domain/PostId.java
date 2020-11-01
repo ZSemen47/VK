7ceeef9fc6423e36544c163d745a072afa25c299
Semen Zazulin <zazulinsemen@gmail.com>
2020-11-01 18:34:19 +0300

    Add PostManager

--- src/main/java/domain/PostId.java
+++ src/main/java/domain/PostId.java
@@ -9,4 +9,62 @@ public class PostId {
     private int replyPostId;
     private int postOpenedId;
     private int signerId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+    public int getReplyOwnerID() {
+        return replyOwnerID;
+    }
+
+    public void setReplyOwnerID(int replyOwnerID) {
+        this.replyOwnerID = replyOwnerID;
+    }
+    public int getReplyPostId() {
+        return replyPostId;
+    }
+
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
+    }
+    public int getPostOpenedId() {
+        return postOpenedId;
+    }
+
+    public void setPostOpenedId(int postOpenedId) {
+        this.postOpenedId = postOpenedId;
+    }
+    public int getSignerId() {
+        return signerId;
+    }
+
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
+    }
 }
---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,14 @@
+package manager;
+
+import domain.PostId;
+
+public class PostManager {
+    private PostId[] posts;
+
+    public PostId[] search(int ownerId, String domain, String query, int ownerOnly, int count,int offset) {
+        return posts;
+    }
+
+    public void delete(int postId, int ownerId) {
+    }
+}
